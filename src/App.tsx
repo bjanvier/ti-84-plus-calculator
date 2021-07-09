45fd9a591f866952c36826d45a1c65579582710c
bjanvier <31869496+bjanvier@users.noreply.github.com>
2021-07-08 20:12:50 -0400

    Completed styling the header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Functions from './functions/Functions';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <div className="container">
+          <div className="header">
+            <section>
+              <div>
+                <h1>TI-84 Plus</h1>
+                <h2>TEXAS INSTRUMENTS</h2>
+              </div>
+              <div className="playground">
+              </div>
+            </section>
+            <section>
+              <ul>
+                <Functions/>
+              </ul>
+            </section>
+          </div>
+          <div className="content">
+            <section className="header-1">
+              </section>
+
+              <section className="header-2">
+              </section>
+                  
+              <section className="advanced-operations">
+              </section>
+              
+              <section className="basics-operations">
+              </section>
+          </div>
+
+       </div>
+       </Router>
     </div>
   );
 }
---
+++ src/functions/Functions.tsx
@@ -0,0 +1,85 @@
+import React, {Component} from "react";
+
+export interface FunctionsProps {
+}
+
+interface FunctionsInfo{
+  label:string,
+  setPlots: any,
+}
+ 
+export interface FunctionsStates {
+  allFunctions:any[]
+}
+ 
+class Functions extends React.Component<FunctionsProps, FunctionsStates> {
+  constructor(props: FunctionsProps) {
+    super(props);
+    this.state = {
+      allFunctions:[
+        {
+          label:"STAT PLOT F1",
+          setPlots: <button   onClick={()=>this.f1()}>Y=</button>
+        },
+        {
+          label:"TBLSET F2",
+          setPlots: <button onClick={()=>this.f2()}>Window=</button>
+        },
+        {
+          label:"FORMAT F3",
+          setPlots: <button onClick={()=>this.f3()}>Zoom</button>
+        },
+        {
+          label: "CALC F4",
+          setPlots: <button onClick={()=>this.f4()}>TRACE=</button>
+        },
+        {
+          label:"TABLE F5",
+          setPlots: <button onClick={()=>this.f5()}>GRAPH</button>
+        }
+      ]
+    };
+  }
+
+  f1(): void {
+    alert("Yan, it works")
+  }
+
+  f2(): void {
+    alert("Yan, it works")
+  }
+
+  f3(): void {
+    alert("Yan, it works")
+  }
+
+  f4(): void {
+    alert("Yan, it works")
+  }
+
+  f5(): void {
+    alert("Yan, it works")
+  }
+
+  render() {
+    const { allFunctions } = this.state;
+    return ( 
+      <>  
+          {
+            allFunctions.map(func => {
+              return (
+                <li>
+                  <strong>
+                   {func.label}
+                  </strong><br/>
+                  {func.setPlots}
+                </li>
+                )
+            })
+          }
+      </>
+     );
+  }
+}
+
+export default Functions;
